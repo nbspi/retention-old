0b7ce497fadea9bc4b53bdabf412a2e5df9e8cfe
jeromedeborja <61317494+jeromedeborja@users.noreply.github.com>
2020-06-22 09:34:02 +0800

    [adj] Contract and Retention Report
    [adj] Contract and Retention Report

--- webapp/controller/ContractReport.controller.js
+++ webapp/controller/ContractReport.controller.js
@@ -44,8 +44,14 @@ sap.ui.define([
 			this.getView().setModel(this.oMdlButtons, "buttons");
 
 		//Vendor Code
-		    this.VendorCode = "";
+			this.VendorCode = "";
+		// To Check if the Generate button was clicked if the value is 1 = Clicked else  0 not yet click
+			this.Generate  = "0";
 
+		//Get All Contract and Retention Records
+			this.oMdlAllRecords = new JSONModel();
+		//Row Count
+		    this.RowCount = 0;
 
 	},
 	Export:function(){
@@ -54,7 +60,6 @@ sap.ui.define([
 		doc.text(20, 20, 'Hello world.');
 		doc.output('Test.pdf');
 
-		// doc.save('Test.pdf');
 
 	},
 
@@ -204,7 +209,93 @@ sap.ui.define([
 			this.getView().byId("ProjCode").setValue(CardDetails[0].ProjectCode);
 	},
 	//------------------- Project Code End -----------------//
+	//--Print
+	fPrint:function(){
 
+		if (this.Generate === "1"){
+			var doc = new jsPDF('l');
+		
+			doc.autoTable({html:'#tblTransaction'});
+			var columns = ["CONTRACTOR","PROJECT DESCRIPTION","DATE STARTED","DATE COMPLETED","DATE LAST PAYMENT","CONTRACT AMOUNT","PAYMENT","BALANCE","ACCOMPLISHMENT","STATUS"];
+			var data = [];
+			for(var i=0;i<this.RowCount;i++)
+					{
+							data[i]=[this.oMdlAllRecords.getData().allData[i].CardName,this.oMdlAllRecords.getData().allData[i].U_APP_ProjCode,this.oMdlAllRecords.getData().allData[i].Date_Started,this.oMdlAllRecords.getData().allData[i].DateCompleted,this.oMdlAllRecords.getData().allData[i].Date_Last_Payment,
+							(this.oMdlAllRecords.getData().allData[i].DocTotal).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),(this.oMdlAllRecords.getData().allData[i].Payment).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),(this.oMdlAllRecords.getData().allData[i].Balance).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),this.oMdlAllRecords.getData().allData[i].Accomplishment,this.oMdlAllRecords.getData().allData[i].Status];
+					}
+			doc.autoTable(columns,data);
+			
+			 // doc.autoTable({
+			 // 	head: [['Contractor']],
+			 // 	body: [
+			// 		 ["oke"],
+			// 		 ["oke"]
+			 // 	]
+			 // });
+			 // doc.autoTable({
+			 // 	head: [['Retention No CWIP','Gross Amount','Prorated Retention','Progress Billing Amount','Prorated DP','Witholding Tax']],
+			 // 	body: [
+			 // 		[RetenNCWIP,GrossAmount,ProratedReten,ProgBillAmount,ProratedDP,WTaX],
+			 // 	]
+			 // });
+			 //doc.text(20, 20, 'Hello world.');	
+			 doc.save('BFI COntract Status Report.pdf');
+			 this.Generate === "0";	
+		}else{
+			sap.m.MessageToast.show("Generate First...");
+			this.Generate === "0";
+		}
+
+
+
+	},
+	//--Generate Process
+	fGenerate:function (){
+		this.Generate = "1";
+		var Contractor = this.VendorCode;
+		var ProjectCode = this.getView().byId("ProjCode").getValue();
+		var StartDate =  this.getView().byId("DateFrom").getValue();
+		var EndDate =  this.getView().byId("DateEnd").getValue();
+
+		if  (Contractor === "" &&  ProjectCode == "" && StartDate == "" && EndDate == ""  ){
+			sap.m.MessageToast.show("No Data to Generate.");
+		}else{
+			if  (Contractor != "" &&  ProjectCode == "" && StartDate == "" && EndDate == ""  ){
+				this.fGetAllRecord("getAllDataContractOnly",Contractor,"","","");
+			}else{
+	
+			}
+		}
+
+
+
+
+
+	},
+	//--Get All Data Report
+	fGetAllRecord:function(QueryTag,value1,value2,value3,value4){
+
+		$.ajax({
+			url: "https://18.136.35.41:4300/app_xsjs/ExecQuery.xsjs?dbName=" + this.Database + "&procName=spAppRetention_Report&queryTag=" + QueryTag +"&value1=" + value1 + "&value2=" + value2 + "&value3=" + value3 + "&value4="+ value4 ,
+			type: "GET",
+			beforeSend: function(xhr) {
+			  xhr.setRequestHeader("Authorization", "Basic " + btoa("SYSTEM:P@ssw0rd805~"));
+			},
+			error: function(xhr, status, error) {
+			  MessageToast.show(error);
+			},
+			success: function(json) {},
+			context: this
+		  }).done(function(results) {
+			if (results) {
+			  this.oMdlAllRecords.setJSON("{\"allData\" : " + JSON.stringify(results) + "}");
+			  this.getView().setModel(this.oMdlAllRecords, "oMdlAllRecords");
+			  this.RowCount = results.length;
+			}
+		  });
+
+
+	}
 
 
   });
